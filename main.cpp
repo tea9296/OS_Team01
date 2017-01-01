4349fa034dd571a7c1fc5ef1f2b30c06591172e4
s10319 <tsaihanlung841203@gamil.com>
2017-01-01 11:21:31 +0800

    light_int
    int match,tobacoo,paper
    int light_s1,light_s1,light_s3
    use to link with Qt

--- main.cpp
+++ main.cpp
@@ -3,99 +3,144 @@
 #include <cstdlib>
 #include <mutex>
 #include <windows.h>
+#include <cstdlib>
 using namespace std;
 
-int match=0;
-int tobacco=0;
-int paper=0;
-int Signal=1, signalM=0, signalT=0, signalP=0;
+int agent=-1;
+
+//light_there
+int match=0;//light_match
+int tobacco=0;//light_tobacco
+int paper=0;//light_paper
+int light_s1=0;//S_match
+int light_s2=0;//S_tobacco
+int light_s3=0;//S_paper
+
+
 mutex agent_Mutex;
-mutex smoker_Mutex;
-void agent(){
+mutex agent_Mutex1;
+mutex agent_Mutex2;
+mutex agent_Mutex3;
+mutex smoker_Mutex1;
+mutex smoker_Mutex2;
+mutex smoker_Mutex3;
+
+void rAgent(){
     while(1){
-	int num;
-    agent_Mutex.lock();
-    if(match+tobacco+paper==0 && Signal==1){
-        num=rand()%3;
-        if(num==0){
-            match=1;
-            tobacco=1;
+        agent_Mutex.lock();
+        agent = rand()%3;
+        if(agent==0){
+            agent_Mutex1.unlock();//match,tobacco
         }
-        else if(num==1){
-            match=1;
-            paper=1;
+        else if(agent==1){
+            agent_Mutex2.unlock();//match,paper
         }
-        else if(num==2){
-            tobacco=1;
-            paper=1;
+        else if(agent==2){
+            agent_Mutex3.unlock();//tobacoo,paper
         }
-        else
-            cout << "err" << endl;
-        cout << match << " " << tobacco << " " << paper << endl;
-        signalM=1;
-        signalT=1;
-        signalP=1;
-        Signal=0;
     }
-    Sleep(1000);
-	smoker_Mutex.unlock();
+}
+
+void agent_1(){
+    while(1){
+        agent_Mutex1.lock();
+        if(agent==0 && match+tobacco+paper==0){
+            match=1;
+            tobacco=1;
+            cout << endl;
+            cout << "|||||||||||||||||\n";
+            cout << "| match tobacco |\n";
+            cout << "|||||||||||||||||\n";
+            Sleep(2000);
+            smoker_Mutex1.unlock();
+            smoker_Mutex2.unlock();
+            smoker_Mutex3.unlock();
+        }
+    }
+}
+void agent_2(){
+    while(1){
+        agent_Mutex2.lock();
+        if(agent==1 && match+tobacco+paper==0){
+            match=1;
+            paper=1;
+            cout << endl;
+            cout << "|||||||||||||||||\n";
+            cout << "| match   paper |\n";
+            cout << "|||||||||||||||||\n";
+            Sleep(2000);
+            smoker_Mutex1.unlock();
+            smoker_Mutex2.unlock();
+            smoker_Mutex3.unlock();
+        }
+    }
+}
+void agent_3(){
+    while(1){
+        agent_Mutex3.lock();
+        if(agent==2 && match+tobacco+paper==0){
+            tobacco=1;
+            paper=1;
+            cout << endl;
+            cout << "|||||||||||||||||\n";
+            cout << "| tobacco paper |\n";
+            cout << "|||||||||||||||||\n";
+            Sleep(2000);
+            smoker_Mutex1.unlock();
+            smoker_Mutex2.unlock();
+            smoker_Mutex3.unlock();
+        }
     }
 }
 
 void smoke_match(){
 	while(1){
-        smoker_Mutex.lock();
-		if(tobacco==1 && paper==1 && signalM==1){
-			cout << "match smoke!" << endl;
+        smoker_Mutex1.lock();
+		if(tobacco==1 && paper==1){
+            light_s1=1;
+            cout << "Hey, I'm match.\n";
 			tobacco=0;
 			paper=0;
-			signalM=0;
-			Signal=1;
+			cout << "goto smoke!\n";
+			Sleep(2000);
+			cout << "Agent wake up!\n";
+			light_s1=0;
+            agent_Mutex.unlock();
 		}
-		else if(Signal==1){
-			//cout << "match can't smoke!" << endl;
-			signalM=0;
-		}
-        agent_Mutex.unlock();
-
 	}
 }
 
 void smoke_tobacco(){
 	while(1){
-        smoker_Mutex.lock();
-		if(match==1 && paper==1 && signalT==1){
-			cout << "tobacco smoke!" << endl;
+        smoker_Mutex2.lock();
+		if(match==1 && paper==1){
+            light_s2=1;
+            cout << "Hey, I'm tobacco.\n";
 			match=0;
 			paper=0;
-			signalT=0;
-			Signal=1;
+			cout << "goto smoke!\n";
+			Sleep(2000);
+			cout << "Agent wake up!\n";
+			light_s2=0;
+            agent_Mutex.unlock();
 		}
-		else if(Signal==1){
-			//cout << "tobacco can't smoke!" << endl;
-			signalT=0;
-		}
-        agent_Mutex.unlock();
-
 	}
 }
 
 void smoke_paper(){
 	while(1){
-            smoker_Mutex.lock();
-		if(match==1 && tobacco==1 && signalP==1){
-			cout << "paper smoke!" << endl;
+        smoker_Mutex3.lock();
+		if(match==1 && tobacco==1){
+            light_s3=1;
+            cout << "Hey, I'm paper.\n";
 			match=0;
 			tobacco=0;
-			signalP=0;
-			Signal=1;
+			cout << "goto smoke!\n";
+			Sleep(2000);
+			cout << "Agent wake up!\n";
+			light_s3=0;
+			agent_Mutex.unlock();
 		}
-		else if(Signal==1){
-			//cout << "paper can't smoke!" << endl;
-			signalP=0;
-		}
-        agent_Mutex.unlock();
-
 	}
 }
 
@@ -110,15 +155,27 @@ int main(){
 	}
 	*/
 
-
+    cout << "Check\n";
+    smoker_Mutex1.lock();
+    smoker_Mutex2.lock();
+    smoker_Mutex3.lock();
+    agent_Mutex1.lock();
+    agent_Mutex2.lock();
+    agent_Mutex3.lock();
+    thread mThreadA(rAgent);
+    thread mThreadA_1(agent_1);
+	thread mThreadA_2(agent_2);
+	thread mThreadA_3(agent_3);
 	thread mThreadM(smoke_match);
 	thread mThreadT(smoke_tobacco);
 	thread mThreadP(smoke_paper);
-	thread mThread1(agent);
-	cout << "Are you serious?"<< endl;
-	mThread1.join();
+	//thread mThread1(agent);
+	mThreadA.join();
 	mThreadM.join();
 	mThreadT.join();
 	mThreadP.join();
+	mThreadA_1.join();
+	mThreadA_2.join();
+	mThreadA_3.join();
 	return 0;
 }
